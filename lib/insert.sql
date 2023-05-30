c850633dd8e673a0603b0f0281bc3a2bca102ec8
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-05-30 11:45:25 +0300

    Complete

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES
+  (1, 'Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1),
+  (2, 'Rowdy', 3, 'M', 'Black', 'Playful', 1),
+  (3, 'Tabitha', 4, 'F', 'Gray', 'Calm', 1),
+  (4, 'Sergeant Brown', 6, 'M', 'Brown', 'Serious', 1),
+  (5, 'Melissa', 2, 'F', 'White', 'Energetic', 1),
+  (6, 'Grinch', 7, 'M', 'Green', 'Mischievous', 1),
+  (7, 'Wendy', 4, 'F', 'Brown', 'Gentle', 1),
+  (8, 'unnamed', NULL, NULL, NULL, NULL, NULL);
